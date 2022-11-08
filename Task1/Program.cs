36843c62536a11872e37042eebe7a48bdcbd781f
Tymofiy Regenel <vt222_rtyu@student.ztu.edu.ua>
2022-11-08 22:12:10 +0200

    Приправлено гумором

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("Git... Він як Старий Заповіт");
 Console.WriteLine("Важко зрозуміти");
 Console.WriteLine("Але без нього в житті ніяк");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("І не сваріть лінивих людей. Вони нічого не зробили");
+Console.WriteLine("Та й таке");
